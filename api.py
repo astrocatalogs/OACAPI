079c07f07092ed7d2b4c2f11afcec5a3cd544cd8
guillochon <guillochon@gmail.com>
2017-10-25 04:32:00 -0700

    ENH: Functional!

--- api.py
+++ api.py
@@ -4,40 +4,102 @@ import os
 
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_compress import Compress
 
 # Create a engine for connecting to SQLite3.
 # Assuming salaries.db is in your app root folder
 
 app = Flask(__name__)
+Compress(app)
 api = Api(app)
 
+catdict = {
+    'sne': 'supernovae',
+    'tde': 'tidaldisruptions',
+    'kilonova': 'kilonovae'
+}
+
+catalogs = {}
+
+ac_path = os.path.join('/root', 'astrocats', 'astrocats')
+
+def get_filename(name):
+    return name.replace('/', '_') + '.json'
+
+class Catalogs(Resource):
+    """Return all catalogs."""
+
+    def get(self):
+        """Get result."""
+        return catalogs
 
 class Catalog(Resource):
     """Return whole catalog."""
 
-    global catalog
-
-    def get(self):
+    def get(self, catalog_name):
         """Get result."""
-        return catalog
+        return catalogs.get(catdict.get(catalog_name, ''), {})
+
+
+class FullEvent(Resource):
+    """Return single event."""
+
+    def get(self, catalog_name, event_name, attribute=None):
+        return Event().get(catalog_name, event_name, attribute, True)
 
 
 class Event(Resource):
     """Return single event."""
 
-    global catalog
-
-    def get(self, event_name):
+    def get(self, catalog_name, event_name, attribute=None, full=False):
         """Get result."""
-        return catalog.get(event_name, {})
+        my_cat = None
+        event = None
+        # Prioritize catalog used to initiate query.
+        for cat in catalogs:
+            if cat != catalog_name:
+                continue
+            if event_name in catalogs[cat]:
+                my_cat = cat
+
+        # Try all other catalogs.
+        for cat in catalogs:
+            if cat == catalog_name:
+                continue
+            if event_name in catalogs[cat]:
+                my_cat = cat
+
+        if not my_cat:
+            return {}
+
+        if attribute and not full and catalogs[my_cat].get(event_name, {}).get(attribute):
+            return catalogs[my_cat][event_name][attribute]
+
+        event = json.load(open(os.path.join(
+            ac_path, catdict[my_cat], 'output', 'json',
+            get_filename(event_name)), 'r'))
+
+        if not attribute:
+            if event:
+                return event
+        else:
+            name = list(event.keys())[0]
+            return(event[name].get(attribute, {}))
+
+        return {}
 
 
-api.add_resource(Event, '/event/<string:event_name>')
-api.add_resource(Catalog, '/catalog')
+api.add_resource(Catalogs, '/<string:catalog_name>/catalogs')
+api.add_resource(Catalog, '/<string:catalog_name>/catalog')
+api.add_resource(Event, '/<string:catalog_name>/event/<string:event_name>',
+    '/<string:catalog_name>/event/<string:event_name>/<string:attribute>')
+api.add_resource(FullEvent, '/<string:catalog_name>/event/<string:event_name>/full',
+    '/<string:catalog_name>/event/<string:event_name>/<string:attribute>/full')
 
 if __name__ == '__main__':
-    global catalog
-    json.load(os.path.join(
-        '/root', 'astrocats', 'astrocats', 'tidaldisruptions', 'output',
-        'catalog.min.json'))
+    print('Loading catalog...')
+    for cat in catdict:
+        catalogs[cat] = json.load(open(os.path.join(
+            ac_path, catdict[cat], 'output', 'catalog.min.json'), 'r'))
+        catalogs[cat] = dict(zip([x['name'] for x in catalogs[cat]], catalogs[cat]))
     app.run()
