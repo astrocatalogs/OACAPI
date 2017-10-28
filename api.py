4cd7ac96986a1efff6a19c11e562b3de6a5a7325
guillochon <guillochon@gmail.com>
2017-10-28 12:29:54 -0400

    MAINT: linting and cleanup

--- api.py
+++ api.py
@@ -1,19 +1,19 @@
 """API for the Open Astronomy Catalogs."""
 import json
 import os
-import numpy as np
-from timeit import default_timer as timer
+import re
 from collections import OrderedDict
+from timeit import default_timer as timer
 
+import numpy as np
+from astropy import units as un
+from astropy.coordinates import SkyCoord as coord
+from six import string_types
 from werkzeug.contrib.fixers import ProxyFix
 
 from flask import Flask, Response, request
 from flask_compress import Compress
 from flask_restful import Api, Resource
-from six import string_types
-from astropy.coordinates import SkyCoord as coord
-from astropy import units as un
-import re
 
 # Create a engine for connecting to SQLite3.
 # Assuming salaries.db is in your app root folder
@@ -41,13 +41,16 @@ decregex = re.compile("^[+-]?[0-9]{1,2}:[0-9]{2}(:?[0-9]{2}\.?([0-9]+)?)?$")
 
 
 def is_list(x):
+    """Check if object is a list (but not a string)."""
     return isinstance(x, list) and not isinstance(x, string_types)
+
 
 def listify(x):
     """Return variable in a list if not already a list."""
     if not is_list(x):
         return [x]
     return x
+
 
 def get_filename(name):
     """Return filename for astrocats event."""
@@ -99,13 +102,14 @@ class Catalog(Resource):
             attribute_name=None):
         """Get result."""
         start = timer()
-        result = self.retrieve(catalog_name, event_name, quantity_name, attribute_name, False)
+        result = self.retrieve(catalog_name, event_name,
+                               quantity_name, attribute_name, False)
         end = timer()
         print('Time to perform query: {}s'.format(end - start))
         return result
 
     def retrieve(self, catalog_name, event_name=None, quantity_name=None,
-            attribute_name=None, full=False):
+                 attribute_name=None, full=False):
         """Retrieve data, first trying catalog file then event files."""
         event = None
         use_full = full
@@ -115,8 +119,9 @@ class Catalog(Resource):
         if not use_full:
             rfull = request.values.get('full')
             if rfull is not None:
-                return self.retrieve(catalog_name, ename, quantity_name, attribute_name, True)
-        fmt = request.values.get('format')
+                return self.retrieve(
+                    catalog_name, ename, quantity_name, attribute_name, True)
+            fmt = request.values.get('format')
         fmt = fmt.lower() if fmt is not None else fmt
 
         ra = request.values.get('ra')
@@ -144,32 +149,40 @@ class Catalog(Resource):
         if ename is None:
             if ra is not None and dec is not None:
                 lcoo = coord(ra, dec, unit=(un.hourangle, un.deg))
-                idxcat = np.where(lcoo.separation(coo) <= radius * un.arcsecond)[0]
+                idxcat = np.where(lcoo.separation(coo) <=
+                                  radius * un.arcsecond)[0]
                 if len(idxcat):
-                    ename = '+'.join([rdnames[i].replace('+', '$PLUS$') for i in idxcat])
+                    ename = '+'.join([rdnames[i].replace('+', '$PLUS$')
+                                      for i in idxcat])
             if ename is None:
                 return catalogs.get(catalog_name, {})
 
-        if fmt is not None and (ename is None or
-                quantity_name is None or attribute_name is None):
+        if fmt is not None and (
+                ename is None or quantity_name is None or
+                attribute_name is None):
             return Response((
                 'Error: "{}" format only supported if event name, quantity, '
                 'and attribute are specified (e.g. '
-                'SN2014J/photometry/magnitude).').format(fmt), mimetype='text/plain')
+                'SN2014J/photometry/magnitude).').format(
+                    fmt), mimetype='text/plain')
         # Events
         event_names = [] if ename is None else ename.split('+')
         event_names = [x.replace('$PLUS$', '+') for x in event_names]
 
         # Quantities
-        quantity_names = [] if quantity_name is None else quantity_name.split('+')
+        quantity_names = [
+        ] if quantity_name is None else quantity_name.split('+')
 
         # Attributes
-        attribute_names = [] if attribute_name is None else attribute_name.split('+')
+        attribute_names = [
+        ] if attribute_name is None else attribute_name.split('+')
 
         if use_full and len(event_names) > self._FULL_LIMIT:
-            return {'message': 'Maximum event limit ({}) exceeded'.format(self._FULL_LIMIT)}
+            return {'message': 'Maximum event limit ({}) exceeded'.format(
+                self._FULL_LIMIT)}
 
-        if fmt is not None and any([n in attribute_names for n in self._NO_CSV]):
+        if fmt is not None and any([n in attribute_names
+                                    for n in self._NO_CSV]):
             return {'message': 'This query does not support delimited output'}
 
         if len(event_names) > self._EXPENSIVE_LIMIT:
@@ -193,9 +206,11 @@ class Catalog(Resource):
             if full:
                 if not my_cat:
                     print(event, my_event)
-                fcatalogs.update(json.load(open(os.path.join(
-                    ac_path, catdict[my_cat], 'output', 'json',
-                    get_filename(my_event)), 'r'), object_pairs_hook=OrderedDict))
+                fcatalogs.update(json.load(
+                    open(os.path.join(
+                        ac_path, catdict[my_cat], 'output', 'json',
+                        get_filename(my_event)), 'r'),
+                    object_pairs_hook=OrderedDict))
             if quantity_name is None:
                 if full:
                     edict[event] = fcatalogs.get(my_event, {})
@@ -233,10 +248,12 @@ class Catalog(Resource):
                 edict[event] = qdict
 
         if not full and use_full:
-            return self.retrieve(catalog_name, ename, quantity_name, attribute_name, True)
+            return self.retrieve(
+                catalog_name, ename, quantity_name, attribute_name, True)
 
         if fmt is not None:
-            return self.get_dsv(edict, event_names, quantity_names, attribute_names, fmt)
+            return self.get_dsv(
+                edict, event_names, quantity_names, attribute_names, fmt)
 
         return edict
 
@@ -260,6 +277,7 @@ class Catalog(Resource):
         return attributes
 
     def get_dsv(self, edict, enames, qnames, anames, fmt='csv'):
+        """Get delimited table."""
         if fmt not in ['csv', 'tsv']:
             return Response('Unknown format.', mimetype='text/plain')
         # Determine which to use as axes in CSV/TSV file.
@@ -317,13 +335,14 @@ class Catalog(Resource):
                     [edict[e].get(q, '') for q in edict[
                         e]] for e in edict]
                 outarr = [[[delim.join(a) if is_list(a) else a
-                    for a in q] for q in e] for e in outarr]
+                            for a in q] for q in e] for e in outarr]
                 outarr = [[delim.join(q) if is_list(q) else q
-                    for q in e] for e in outarr]
+                           for q in e] for e in outarr]
             elif cax == 'a':
-                #outarr = [[i for s in edict[e][qname]
+                # outarr = [[i for s in edict[e][qname]
                 #           for i in s] for e in edict]
-                outarr = [i for s in [[[enames[ei]] + q for q in edict[e][qname]]
+                outarr = [i for s in [
+                    [[enames[ei]] + q for q in edict[e][qname]]
                     for ei, e in enumerate(edict)] for i in s]
             else:
                 outarr = [edict[e][qname] for e in edict]
@@ -349,7 +368,7 @@ class Catalog(Resource):
                   for y in outarr]
 
         if cax is None:
-            #if rax is None:
+            # if rax is None:
             #    outarr = list(map(list, zip(*outarr)))
             cax, rax = rax, None
             colheaders, rowheaders = list(rowheaders), None
@@ -364,20 +383,25 @@ class Catalog(Resource):
             [delim.join(y) for y in outarr]), mimetype='text/plain')
 
 
-#api.add_resource(Info, '/<string:catalog_name>/')
-api.add_resource(Catalogs, '/<string:catalog_name>/catalogs')
+cn = '<string:catalog_name>'
+en = '<string:event_name>'
+qn = '<string:quantity_name>'
+an = '<string:attribute_name>'
+
+# api.add_resource(Info, '/<string:catalog_name>/')
+api.add_resource(Catalogs, '/'.join(['', cn, 'catalogs']))
 api.add_resource(
     Catalog,
-    '/<string:catalog_name>',
-    '/<string:catalog_name>/catalog',
-    '/<string:catalog_name>/all/<string:quantity_name>',
-    '/<string:catalog_name>/all/<string:quantity_name>/<string:attribute_name>',
-    '/<string:catalog_name>/event/<string:event_name>',
-    '/<string:catalog_name>/event/<string:event_name>/<string:quantity_name>',
-    '/<string:catalog_name>/event/<string:event_name>/<string:quantity_name>/<string:attribute_name>',
-    '/<string:catalog_name>/<string:event_name>',
-    '/<string:catalog_name>/<string:event_name>/<string:quantity_name>',
-    '/<string:catalog_name>/<string:event_name>/<string:quantity_name>/<string:attribute_name>')
+    '/'.join(['', cn]),
+    '/'.join(['', cn, 'catalog']),
+    '/'.join(['', cn, 'all', qn]),
+    '/'.join(['', cn, 'all', qn, an]),
+    '/'.join(['', cn, 'event', en]),
+    '/'.join(['', cn, 'event', en, qn]),
+    '/'.join(['', cn, 'event', en, qn, an]),
+    '/'.join(['', cn, en]),
+    '/'.join(['', cn, en, qn]),
+    '/'.join(['', cn, en, qn, an]))
 
 if __name__ == '__main__':
     print('Loading catalog...')
@@ -410,13 +434,6 @@ if __name__ == '__main__':
             rdnames.append(event)
             ras.append(lra)
             decs.append(ldec)
-
-    #for i, ra in enumerate(ras):
-    #    try:
-    #        temp = coord(ra, decs[i], unit=(un.hourangle, un.deg))
-    #    except Exception as e:
-    #        print(rdnames[i], ra, decs[i])
-    #        raise e
 
     coo = coord(ras, decs, unit=(un.hourangle, un.deg))
 
